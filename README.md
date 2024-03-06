074cfaa53d9153762769b02e42f513a9d817ae84
bellicosi-in <73630139+bellicosi-in@users.noreply.github.com>
2024-03-06 23:53:58 +0530

    Hello World for the BIOS

---
+++ README.md
@@ -0,0 +1,7 @@
+#PINPOG WITHOUT OS
+
+## writing a game without the os. 
+
+dependencies : qemu , nasm
+
+resources : https://www.cs.bham.ac.uk/~exr/lectures/opsys/10_11/lectures/os-dev.pdf
--- pinpog.asm
+++ pinpog.asm
@@ -1,5 +1,18 @@
-loop:
-    jmp loop
+    mov ah, 0x0e ;function number = 0Eh: Display character
+
+    mov al, 'H' ; code of character to display
+    int 0x10 ; BIOS video interrupt
+    mov al, 'e'
+    int 0x10
+    mov al, 'l'
+    int 0x10
+    mov al, 'i'
+    int 0x10
+    mov al, 'o'
+    int 0x10
+
+
+    jmp $ ; jmp loop
 
     times 510-($-$$) db 0 ;$ - current address $$-start address.
 
