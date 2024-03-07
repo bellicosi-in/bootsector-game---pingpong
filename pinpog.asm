2421dfd325da7285f888fe8ae3729f00f280836d
bellicosi-in <73630139+bellicosi-in@users.noreply.github.com>
2024-03-07 10:04:56 +0530

    trying to get the display right

--- pinpog.asm
+++ pinpog.asm
@@ -1,18 +1,36 @@
-    mov ah, 0x0e ;function number = 0Eh: Display character
+    mov ah, 0x00
+    ;VGA mode 0x13
+    ;320x200 256 colors
 
-    mov al, 'H' ; code of character to display
-    int 0x10 ; BIOS video interrupt
-    mov al, 'e'
+    mov al,0x13
     int 0x10
-    mov al, 'l'
-    int 0x10
-    mov al, 'i'
-    int 0x10
-    mov al, 'o'
-    int 0x10
+    
+    mov ax, 0xA000
+    mov es, ax
+    mov bx,0
+
+loop:
+    mov BYTE [es:bx], 0x0F
+    inc ax
+    cmp ax, 64000
+    jb loop
 
 
-    jmp $ ; jmp loop
+    ;mov ah, 0x0e ;function number = 0Eh: Display character
+
+    ;mov al, 'H' ; code of character to display
+    ;int 0x10 ; BIOS video interrupt
+    ;mov al, 'e'
+    ;int 0x10
+    ;mov al, 'l'
+    ;int 0x10
+    ;mov al, 'i'
+    ;int 0x10
+    ;mov al, 'o'
+    ;int 0x10
+
+
+    ;jmp $ ; jmp loop
 
     times 510-($-$$) db 0 ;$ - current address $$-start address.
 
